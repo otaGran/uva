0c8465731d37d15a66b5f1dffff10fb1b86f1068
beans_pc <ujt2h8@gmail.com>
2018-02-16 19:29:29 +0800

    update 8.2.3_Onlogn_bsearch.cpp

--- AOAPC/8.3_qipanfugai.cpp
+++ AOAPC/8.3_qipanfugai.cpp
@@ -1,7 +1,7 @@
 //
 // Created by beans_pc on 2/16/2018.
 //棋盘覆盖问题
-//有一个2^k*2^k的方格棋盘,恰有一个方格是黑色的,其他为白色,如何用三种L形派覆盖所有的白色格子且任意白色格子只能覆盖一次
+//有一个2^k*2^k的方格棋盘,恰有一个方格是黑色的,其他为白色,如何用四种L形派覆盖所有的白色格子且任意白色格子只能覆盖一次
 
 #include <iostream>
 #include <cstring>
@@ -28,7 +28,7 @@ int getby(int i,int sx,int sy,int k){
 
 void slove(char map[100][100],int k, int bx, int by, int sx, int sy){
     //printf("%d %d %d %d %d\n",k,bx,by,sx,sy);
-    if(k==1){
+    if(k==1){//we can slove this pro when k = 1
         for(int i = sx;i<=sx+1;i++){
             for(int j = sy;j<=sy+1;j++){
                 if(i!=bx||j!=by)
@@ -42,13 +42,13 @@ void slove(char map[100][100],int k, int bx, int by, int sx, int sy){
             int nextx = (i==0||i==2) ? sx : (sx+(int)pow(2,k)/2);
             int nexty = (i==0||i==1) ? sy : (sy+(int)pow(2,k)/2);
             printf("%d %d\n",nextx,nexty);
-            if(bx>=nextx&&bx<nextx+(int)pow(2,k-1) && by>=nexty&&by<nexty+(int)pow(2,k-1)){//already have black pixel
+            if(bx>=nextx&&bx<nextx+(int)pow(2,k-1) && by>=nexty&&by<nexty+(int)pow(2,k-1)){//already have a black pixel
                 slove(map,k-1,bx,by,nextx,nexty);
                 //printf("%d %d %d %d %d\n",k-1,bx,by,nextx,nexty);
             }else{
-                printf("%d %d %d %d %d\n",k-1,getbx(i,nextx,nexty,k-1), getby(i,nextx,nexty,k-1), nextx, nexty);
+                //printf("%d %d %d %d %d\n",k-1,getbx(i,nextx,nexty,k-1), getby(i,nextx,nexty,k-1), nextx, nexty);
                 slove(map, k-1, getbx(i,nextx,nexty,k-1), getby(i,nextx,nexty,k-1), nextx, nexty);
-                map[getbx(i,nextx,nexty,k-1)][ getby(i,nextx,nexty,k-1)] = '#';
+                map[getbx(i,nextx,nexty,k-1)][ getby(i,nextx,nexty,k-1)] = '#';//use '#' to mark virtual pixel
 
             }
 
@@ -62,7 +62,7 @@ int main(){
     cin >> k >> bx >> by;
     char map[100][100];
     memset(map,0,sizeof(map));
-    map[bx][by] = '*';
+    map[bx][by] = '*';//mark for first black pixel
 
 
     slove(map,k,bx,by,0,0);
@@ -72,9 +72,5 @@ int main(){
             printf("%2c ",map[i][j]);
         cout << endl;
     }
-
-    //for(int i = 0;i<4;i++)
-    int i = 0;
-       //printf("%d : (%d, %d)\n",i,getbx(i,0,0,2),getby(i,0,0,2));
 }
 
---
+++ AOAPC/8.3_xun_huan_ri_cheng_biao.cpp
@@ -0,0 +1,74 @@
+//
+// Created by beans_pc on 2/16/2018.
+//循环日程表 
+
+#include <iostream>
+#include <cmath>
+#include <cstring>
+
+using namespace std;
+
+
+void re_slove(int A[100][100],int sx, int sy, int k){
+    if(k==0)
+        return;
+    else{
+        int tmpk = k -1;
+        A[sx][sy+(1<<tmpk)] = A[sx][sy] + (1<<tmpk);
+        A[sx+(1<<tmpk)][sy] = A[sx][sy+(1<<tmpk)];
+        A[sx+(1<<tmpk)][sy+(1<<tmpk)] = A[sx][sy];
+
+        re_slove(A,sx,sy,k-1);
+        re_slove(A,sx+(1<<tmpk),sy,k-1);
+        re_slove(A,sx,sy+(1<<tmpk),k-1);
+        re_slove(A,sx+(1<<tmpk),sy+(1<<tmpk),k-1);
+
+    }
+
+}
+
+void slove(int k){
+    int size = pow(2,k);
+
+    int data[size][size];
+    memset(data,0, sizeof(data));
+
+    data[0][0] = 1;
+
+
+    int tmpSize = 0;
+    while(tmpSize < k){
+        for(int i = 0;i<pow(2,tmpSize);i++){
+            for(int j = 0;j<pow(2,tmpSize);j++){
+                data[i][j+(int)pow(2,tmpSize)] = data[i][j] + (int)pow(2,tmpSize);
+                data[i+(int)pow(2,tmpSize)][j] = data[i][j+(int)pow(2,tmpSize)];
+                data[i+(int)pow(2,tmpSize)][j+(int)pow(2,tmpSize)] = data[i][j];
+            }
+        }
+        tmpSize++;
+
+    }
+    for(int i = 0;i<(1<<k);i++){
+        for(int j = 0;j<(1<<k);j++)
+            printf("%2d ",data[i][j]);
+        cout << endl;
+    }
+}
+
+
+int main(){
+    int k;
+    cin >> k;
+    slove(k);
+    int data[100][100];
+    memset(data,0, sizeof(data));
+    data[0][0] = 1;
+    re_slove(data,0,0,k);
+    cout << endl;
+    for(int i = 0;i<(1<<k);i++){
+        for(int j = 0;j<(1<<k);j++)
+            printf("%2d ",data[i][j]);
+        cout << endl;
+    }
+
+}
